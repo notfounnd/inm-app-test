c0ff2d867fc30e449e70cc73db039c8d9e88a059
Júnior Sbrissa <57534340+notfounnd@users.noreply.github.com>
2020-06-26 10:36:08 -0300

    Update settings.py

--- site_inm/settings.py
+++ site_inm/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
